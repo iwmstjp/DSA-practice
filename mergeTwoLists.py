b4596d27ef6dcad87eb92cc0ce2d7a9eee83b356
iwmstjp <miwaura95@gmail.com>
2023-10-21 07:26:54 +0200

    Added mergeTowList.py

---
+++ mergeTwoLists.py
@@ -0,0 +1,22 @@
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        result = ListNode()
+        head = result
+        while list1 and list2:
+            if list1.val <= list2.val:
+                result.next = list1
+                result = result.next
+                list1 = list1.next
+            else:
+                result.next = list2
+                result = result.next
+                list2 = list2.next
+        while list1:
+            result.next = list1
+            result = result.next
+            list1 = list1.next
+        while list2:
+            result.next = list2
+            result = result.next
+            list2 = list2.next
+        return head.next
